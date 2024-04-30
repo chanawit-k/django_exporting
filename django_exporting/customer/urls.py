daede8b7b9c681f5f8602be19f0143e954169aed
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-04-30 23:19:26 +0700

    - template listview

---
+++ django_exporting/customer/urls.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.views import logout_then_login
+from django.urls import path
+from . import views
+
+app_name = 'customer'
+
+
+urlpatterns = [
+    path('', views.CustomerListView.as_view(), name='customer_list'),
+]
